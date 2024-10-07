4d07b14310db6d5d5d11c5bd1703d6e898a6f7c1
YousefAlam12 <alamyousef32@gmail.com>
2024-10-07 14:05:11 +0100

    fixed falling over bug

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,56 +5,6 @@ using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
-    //public Vector2 moveValue;
-
-    //[SerializeField]
-    //private float moveSpeed;
-
-    //[SerializeField]
-    //private float maxSpeed;
-
-    //[SerializeField]
-    //private float jumpSpeed;
-
-    //private Vector3 forceDirection;
-
-    //private Rigidbody rb;
-
-
-    //void Awake()
-    //{
-    //    rb = GetComponent<Rigidbody>();
-    //    inputActions = new InputSystem();
-    //}
-
-    //void OnEnable()
-    //{
-    //    inputActions.Land.Jump.performed += OnJump;
-    //    move = inputActions.Land.Movement;
-    //    inputActions.Enable();
-    //}
-
-    //void OnDisable()
-    //{
-    //    inputActions.Land.Jump.performed -= OnJump;
-    //    inputActions.Disable();
-    //}
-
-    //void OnJump(InputAction.CallbackContext ctx)
-    //{
-
-    //}
-
-    //void FixedUpdate()
-    //{
-    //    forceDirection += moveSpeed.ReadValue<Vector2>().x * transform.right * moveSpeed;
-    //    forceDirection += moveSpeed.ReadValue<Vector2>().y * transform.forward * moveSpeed;
-    //    rb.AddForce(forceDirection, ForceMode.Impulse);
-    //    forceDirection += Vector3.zero;
-
-
-    //}
-
     PlayerInput playerInput;
     InputAction moveAction;
     public float moveSpeed;
@@ -78,7 +28,4 @@ public class MovementScript : MonoBehaviour
         Vector2 direction = moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * moveSpeed * Time.deltaTime;
     }
-
-
-
 }
