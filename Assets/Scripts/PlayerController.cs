95bcfdd8338cd82c6258a8525f630c84306480a3
Yousuf521 <yousufahmedismaeel@gmail.com>
2024-10-11 18:10:40 +0100

    Revert "Merge branch 'main' into Yousuf"
    This reverts commit 57ce384b229d4f341d4b32a21eeb9532457ccf92, reversing
    changes made to 72d35b220a59672fb8caae1c8fa0f42e8f45c187.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,31 +9,30 @@ public class PlayerController : MonoBehaviour
     // InputAction moveAction;
     // private Rigidbody rb;
     public float moveSpeed;
-    public float sprintSpeed;
-    public float walkSpeed;
-
     public float jumpForce;
-    public float gravityMultiplier;
-
     public CharacterController controller;
     private Vector3 moveDirection;
-
-    
-
- 
+    public float gravityMultiplier;
 
     void Start()
     {
+        // using InputSystem
+        // playerInput = GetComponent<PlayerInput>();
+        // moveAction = playerInput.actions.FindAction("Move");
+
         // using rigid body
         // rb = GetComponent<Rigidbody>();
 
         // using CharacterController
         controller = GetComponent<CharacterController>();
-        moveSpeed = walkSpeed;
     }
 
     void MovePlayer()
-    {       
+    {
+        // Vector2 direction = moveAction.ReadValue<Vector2>();
+        // transform.position += new Vector3(direction.x, 0, direction.y) * moveSpeed * Time.deltaTime;
+        
+        
         // using rigid body
         // rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
 
@@ -43,31 +42,13 @@ public class PlayerController : MonoBehaviour
 
 
         // using CharacterController for 3D movement 
-
-        // moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
-
-        // stores the y position of the player before calculating the movement of player with mouse in order allow jumps
-        float yStore = moveDirection.y;
-
-        // using the mouse to move the player in chosen directions
-        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
-        moveDirection = moveDirection.normalized * moveSpeed;
-        moveDirection.y = yStore;
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
 
         // preventing infinite jumps
         if(controller.isGrounded) {
-            moveDirection.y = 0f;
 
             if(Input.GetButtonDown("Jump")) {
                 moveDirection.y = jumpForce; 
-            }
-
-            // sprint button
-            if(Input.GetButton("Fire3")) {
-                    moveSpeed = sprintSpeed;
-                }
-            else {
-                moveSpeed = walkSpeed;
             }
         }
 
@@ -83,4 +64,7 @@ public class PlayerController : MonoBehaviour
     {
         MovePlayer();
     }
+
+
+
 }
