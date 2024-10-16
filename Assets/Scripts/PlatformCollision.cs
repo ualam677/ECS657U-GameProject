d2bc371c6d15f92e86e6e9fb4a41da81c009827d
Yousuf521 <yousufahmedismaeel@gmail.com>
2024-10-16 19:31:20 +0100

    fixed platform collision

---
+++ Assets/Scripts/PlatformCollision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCollision : MonoBehaviour
+{
+    [SerializeField] string playerTag = "Player";
+    [SerializeField] Transform platform;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals(playerTag))
+        {
+            other.gameObject.transform.parent = platform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals(playerTag))
+        {
+            other.gameObject.transform.parent = null;
+        }
+    }
+}
