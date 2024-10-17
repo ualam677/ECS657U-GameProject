b490d0cd3ef0cc61e1954182cc7d72e29eda0886
Yaqu0b <yaquobrahman@gmail.com>
2024-10-17 16:35:49 +0100

    Added Game Over screen and Restart button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    bool isGameOver = false;
-    public float restartDelay = 2f;
-    
-    public void EndGame()
+    public HealthManager healthManager;
+    public GameObject mainCamera;
+    public GameObject gameOverPanel;
+    public Button restartButton;
+    public TextMeshProUGUI gameOverText; 
+
+    void Start() 
     {
-        if (isGameOver == false)
-        {
-            isGameOver = true;
-            Debug.Log("GAME OVER");
-            Invoke("Restart", restartDelay);
+        // Hide Game Over panel initially
+        gameOverPanel.SetActive(false);
+        gameOverText.gameObject.SetActive(false);
+
+        healthManager = FindAnyObjectByType<HealthManager>();
+        mainCamera = Camera.main.gameObject;
+    }
+
+    void Update() 
+    {
+        if (healthManager.currentHealth <= 0) {
+            GameOver();
         }
     }
 
-    void Restart()
+    void GameOver() 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // Display Game Over panel
+        gameOverPanel.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        Time.timeScale = 0f; // Pause the game
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        mainCamera.GetComponent<CameraController>().enabled = false;
     }
 
+    void Awake()
+    {
+        Time.timeScale = 1f; // Resume game on scene load
+    }
 
+    public void RestartGame() 
+    {
+        // Reload the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
